dc22ff74e3d7eb3ed7ddf48de1040a5a27a8a050
Hellon Canella <helloncanella@gmail.com>
2017-01-11 15:12:13 -0200

    added font-awesome

--- src/pages/footer/social-icons.jsx
+++ src/pages/footer/social-icons.jsx
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 import styles from './social-icons.scss'
 import grid from '../../stylesheets/grid.scss'
 
-
 export default class SocialIcons extends Component {
 
     allIcons(){
-        return icons.map((iconClass, index)=><span className={iconClass} key={index}></span>)
+        return [].map((iconClass, index)=><span className={iconClass} key={index}></span>)
     }
 
     render() {
@@ -14,12 +13,10 @@ export default class SocialIcons extends Component {
 
         return (
             <div className={`${icons} ${this.props.className}`}>
-                {this.allIcons()}
+                <i className="fa fa-fw fa-question"></i>
             </div>
         )
     }
 }
 
-const iconsClass = {
-    
-}
+const iconsClass = []
